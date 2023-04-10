4893c72a27c5355851e81a119ed9d398a82ad291
Nikolas <subedikiran678@gmail.com>
2023-04-11 00:16:05 +0545

    Rectangle added into ememy

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ clock = pygame.time.Clock()
 
 enemy_surf = pygame.Surface((20,20))
 enemy_surf.fill('red')
+enemy_rect = enemy_surf.get_rect(bottomleft = (700,300))
 x_pos = 700
 
 sky_surf = pygame.Surface((800,300))
@@ -31,15 +32,16 @@ while True:
             pygame.quit()
             exit()
 
-    x_pos -= 2
-    if x_pos == -50:
-        x_pos = 850
     screen.blit(sky_surf,(0,0))
     screen.blit(ground_surf,(0,300))
-    screen.blit(enemy_surf,(x_pos,280))
+    screen.blit(text_surf,(350,50))
+
+    enemy_rect.left -= 2
+    if enemy_rect.right <= 0: enemy_rect.left =800
+    screen.blit(enemy_surf,enemy_rect)
+
     player_rect.left += 1
     screen.blit(player_surf,player_rect)
-    screen.blit(text_surf,(350,50))
 
     pygame.display.update()
     clock.tick(60)
