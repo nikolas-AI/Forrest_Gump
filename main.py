cb759309abaa9e96fd3ad7405bff79f51ba43ad7
Nikolas <subedikiran678@gmail.com>
2023-05-28 23:59:50 +0545

    ss

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ while True:
             if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
                     player_gravity = -20
 
-    if game_active:
+    if game_active == True:
         screen.blit(sky_surf,(0,0))
         screen.blit(ground_surf,(0,300))
         pygame.draw.rect(screen, '#c0e8ec',score_rect)
