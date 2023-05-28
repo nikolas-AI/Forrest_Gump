bba4a5ed26992d2369165790eaa40f145efec048
Nikolas <subedikiran678@gmail.com>
2023-05-28 23:45:56 +0545

    ddd

--- main.py
+++ main.py
@@ -36,12 +36,11 @@ while True:
             exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if player_rect.collidepoint(event.pos):
+            if player_rect.collidepoint(event.pos) and  player_rect.bottom >= 300:
                 player_gravity = -20
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                if player_rect.bottom == 300:
+            if event.key == pygame.K_SPACE and  player_rect.bottom >= 300:
                     player_gravity = -20
 
 
