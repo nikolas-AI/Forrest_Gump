f136a395f578698dd07a7739138a89808da67640
Nikolas <subedikiran678@gmail.com>
2023-05-28 23:16:11 +0545

    aliena transformed

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ clock = pygame.time.Clock()
 
 
 enemy_surf = pygame.image.load('aliena.png')
-enemy_rect = pygame.transform.scale(enemy_surf, (50,50))
+enemy_surf = pygame.transform.scale(enemy_surf, (50,50))
 enemy_rect = enemy_surf.get_rect(bottomleft = (700,300))
 x_pos = 700
 
