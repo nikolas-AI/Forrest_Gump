d4114a4d2ceb36b6835a277cf55c1bfd130dcd85
Nikolas <subedikiran678@gmail.com>
2023-05-29 19:26:50 +0545

    ddd

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pygame
 from sys import exit
 
 def display_score():
-   time = int(pygame.time.get_ticks() / 1000) - start_time
-   score_surf = text_font.render(f'{time}', False, (64, 64, 64))
+   time = int(pygame.time.get_ticks() / 800) - start_time
+   score_surf = text_font.render(f'{time}', False, 'Blue')
    score_rect = score_surf.get_rect(center = (400, 60))
    screen.blit(score_surf, score_rect)
 
@@ -27,18 +27,22 @@ sky_surf = pygame.image.load('screen2.jpg')
 sky_surf = pygame.transform.scale(sky_surf, (800,800))
 sky_rect = sky_surf.get_rect(x = 0)
 
-ground_surf = pygame.Surface((800,100))
-ground_surf.fill('green')
+ground_surf = pygame.image.load('ground.jpg')
+ground_surf = pygame.transform.scale(ground_surf, (800,800))
+sky_rect = ground_surf.get_rect(y = 0)
 
-text_font = pygame.font.SysFont('Calbiri', 30)
-name_surf = text_font.render('Forrest Gump', False, (64,64,64))
-name_rect = name_surf.get_rect(center = (400,40))
+# ground_surf = pygame.Surface((800,100))
+# ground_surf.fill('green')
+
+text_font = pygame.font.SysFont('Ariel', 40)
+name_surf = text_font.render('Forrest Gump', False, 'Black')
+name_rect = name_surf.get_rect(center = (400,20))
 
 player_surf = pygame.image.load('chara.png')
 player_surf = pygame.transform.scale(player_surf, (70,70))
 player_rect = player_surf.get_rect(bottomleft = (100,300))
 
-restart_surf = pygame.image.load('screen.jpg')
+restart_surf = pygame.image.load('screen1.jpg')
 restart_surf = pygame.transform.scale(restart_surf, (800, 800))
 restart_rect = restart_surf.get_rect(x = 0)
 
@@ -62,7 +66,7 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 enemy_rect.left = 800
-                start_time = int(pygame.time.get_ticks() / 1000)
+                start_time = int(pygame.time.get_ticks() / 800)
 
     if game_active:
         screen.blit(sky_surf,(0,0))
