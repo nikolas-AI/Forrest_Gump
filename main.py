5c259e26af47f208857c5dbeb6407567da58b871
Nikolas <subedikiran678@gmail.com>
2023-05-30 19:44:33 +0545

    player group

--- main.py
+++ main.py
@@ -8,7 +8,23 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load('char.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (60, 60))
-        self.rect = self.image.get_rect(bottomleft = (100,360))
+        self.rect = self.image.get_rect(bottomleft = (130,360))
+        self.gravity = 0
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 180:
+            self.gravity = -18
+
+    def apply_gravity(self):
+        self.gravity += 0.8
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 360:
+            self.rect.bottom = 360
+    
+    def update(self):
+        self.player_input()
+        self.apply_gravity()
 
 def display_score():
    time = int(pygame.time.get_ticks() / 800) - start_time
@@ -151,6 +167,7 @@ while True:
             player_rect.bottom = 360
         screen.blit(player_surf, player_rect)
         player.draw(screen)
+        player.update()
 
         #Obstacle movement
         obstacle_rect_list = obstacle_movement(obstacle_rect_list)
